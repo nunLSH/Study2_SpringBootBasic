b85259efb180e5db1c4b98ff7162cf922ebb974f
Seohyeon.Lim <forestlight749@gmail.com>
2024-01-22 15:20:06 +0900

    (추가) 회원 가입 main, 애플리케이션 로직으로 테스트하는 코드

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -2,6 +2,7 @@ package hello.core.member;
 
 public class MemberServiceImpl implements MemberService{
 
+    // MemberServiceImpl는 추상화에도 의존하고, 구체화에도 의존하고 있음 -> DIP 위반
     private final MemberRepository memberRepository = new MemoryMemberRepository();
 
     @Override
