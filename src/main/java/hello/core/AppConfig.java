fa9453474f457ad3eebe63706a94f577bdc59d5c
Seohyeon.Lim <forestlight749@gmail.com>
2024-01-23 12:06:08 +0900

    (리팩터링) AppConfig _ 중복 제거, 역할에 따른 구현 정리

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,7 +1,9 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -11,13 +13,19 @@ import hello.core.order.OrderServiceImpl;
 // 실제 동작에 필요한 구현 객체를 생성
 public class AppConfig {
 
-    public MemberService memberService() {
-        // 생성자 주입(연결) -> DI
-        return new MemberServiceImpl(new MemoryMemberRepository());
+    public MemberService memberService() { // 역할
+        return new MemberServiceImpl(memberRepository()); // 구현
     }
 
-    public OrderService orderService() {
-        // 생성자 주입(연결) -> DI
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+    private static MemberRepository memberRepository() { // 역할
+        return new MemoryMemberRepository(); // 구현
+    }
+
+    public OrderService orderService() { // 역할
+        return new OrderServiceImpl(memberRepository(),discountPolicy()); // 구현
+    }
+
+    public DiscountPolicy discountPolicy() { // 역할
+        return new FixDiscountPolicy(); // 구현
     }
 }
